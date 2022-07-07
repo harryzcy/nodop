2a990f3b31e6d9663f4f2b5ac8084cf89d885504
Chongyi Zheng <harry@harryzheng.com>
2022-07-07 19:03:55 -0400

    Use top-level await in main

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,15 @@ import cache from './cache.js'
 
 const configPath = process.env.CONFIG_PATH || 'nodop'
 
-async function main() {
-  await loadConfig(configPath)
-  const index = getConfigIndex()
+await loadConfig(configPath)
+const index = getConfigIndex()
 
-  await cache.startRun()
+await cache.startRun()
 
-  if (process.argv.includes('--daemon')) {
-    await runNonStop(index)
-  } else {
-    await runOnce(index)
-  }
-
-  await cache.stopRun()
+if (process.argv.includes('--daemon')) {
+  await runNonStop(index)
+} else {
+  await runOnce(index)
 }
 
-main()
+await cache.stopRun()
