070920c417750b712376ac051d4c5705fe1c8e87
Chongyi Zheng <harry@harryzheng.com>
2022-07-08 00:44:43 -0400

    Update default config path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { homedir } from 'os'
+import path from 'path'
 import { loadConfig, getConfigIndex } from './config.js'
 import { runOnce, runNonStop } from './runner.js'
 import cache from './cache.js'
 
-const configPath = process.env.CONFIG_PATH || 'nodop'
+const configPath = process.env.CONFIG_PATH || path.join(homedir(), 'nodop')
 
 await loadConfig(configPath)
 const index = getConfigIndex()
