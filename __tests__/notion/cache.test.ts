4aa17e00ca4356552980cb4e21ef3b7357e75a8a
harryzcy <harry@harryzheng.com>
2022-10-25 18:33:37 -0400

    Improve test coverage

--- __tests__/notion/cache.test.ts
+++ __tests__/notion/cache.test.ts
@@ -1,45 +1,61 @@
 import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints.js'
-import { getPage, putPage, clearPages } from '../../src/notion/cache.js'
+import { getPage, putPage, clearPages, cleanupPages } from '../../src/notion/cache.js'
+
+const page: PageObjectResponse = {
+  object: 'page',
+  parent: {
+    type: 'database_id',
+    database_id: ''
+  },
+  properties: undefined,
+  icon: {
+    type: 'emoji',
+    emoji: '😀'
+  },
+  cover: {
+    type: 'external',
+    external: {
+      url: ''
+    }
+  },
+  created_by: {
+    id: '',
+    object: 'user'
+  },
+  last_edited_by: {
+    id: '',
+    object: 'user'
+  },
+  id: '',
+  created_time: '',
+  last_edited_time: '',
+  archived: false,
+  url: ''
+}
 
 describe('cached pages operations', () => {
   it('put, get, clear, and get', () => {
     clearPages() // reset cache
 
-    const page: PageObjectResponse = {
-      object: 'page',
-      parent: {
-        type: 'database_id',
-        database_id: ''
-      },
-      properties: undefined,
-      icon: {
-        type: 'emoji',
-        emoji: '😀'
-      },
-      cover: {
-        type: 'external',
-        external: {
-          url: ''
-        }
-      },
-      created_by: {
-        id: '',
-        object: 'user'
-      },
-      last_edited_by: {
-        id: '',
-        object: 'user'
-      },
-      id: '',
-      created_time: '',
-      last_edited_time: '',
-      archived: false,
-      url: ''
-    }
-
     putPage(page)
     expect(getPage(page.id)).toBe(page)
     clearPages()
+    expect(getPage(page.id)).toBe(null)
+  })
+
+  it('cleanup', () => {
+    clearPages() // reset cache
+
+    putPage(page)
+    expect(getPage(page.id)).toBe(page)
+
+    // cleanupPages decrements ttl
+    // and removes pages with ttl === 0
+    // so the page should be removed
+    // after two calls to cleanupPages
+    cleanupPages()
+    expect(getPage(page.id)).toBe(page)
+    cleanupPages()
     expect(getPage(page.id)).toBe(null)
   })
 })
